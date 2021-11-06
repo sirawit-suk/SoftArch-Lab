5fb3757085d84b0a58838092cc1d52e5bf3e1acf
Sirawit <popeye-zz@hotmail.com>
2021-11-06 22:58:07 +0700

    finish Strategy Design Pattern

--- Lab10_Behavioral_Design_Pattern_2/SoftArchDesign/src/HeightSortStrategy.java
+++ Lab10_Behavioral_Design_Pattern_2/SoftArchDesign/src/HeightSortStrategy.java
@@ -1,7 +1,12 @@
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+// Reference: https://www.techiedelight.com/sort-list-of-objects-using-comparator-java/
 public class HeightSortStrategy implements SortStrategy {
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
-
+    @Override
+    public void customSort(List<Person> people) {
+        Collections.sort(people, Comparator.comparingInt(Person::getHeight));
+    }
 }
